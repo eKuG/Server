215bee26d0325bedda7ef5dad47dc7f2f946c719
Ekansh Gupta <33174166+eKuG@users.noreply.github.com>
2019-07-05 00:00:53 +0530

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://username:'+ encodeURIComponent<'password'> + 'project.mongodb.net/test?retryWrites=true';
+
+module.exports = {
+    mongoURI: 'mongodb+srv://username:password@project-r5xnf.mongodb.net/test?retryWrites=true&w=majority'
+};
